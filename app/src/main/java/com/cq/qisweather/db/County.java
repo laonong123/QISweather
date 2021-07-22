e4a91d4d6ddff1d69ba7403c808cbea8ed36e214
laonong123 <ncq0908@163.com>
2021-07-22 09:25:23 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/cq/qisweather/db/County.java
+++ app/src/main/java/com/cq/qisweather/db/County.java
@@ -7,7 +7,7 @@ public class County extends LitePalSupport {
     private int id;//每一个实体类都需有一个id
     private String countyName; //记录区县名称
     private int countyCode;//记录区县代码
-    private int weatherId;//记录该区县的天气信息
+    private String weatherId;//记录该区县的天气信息
     private int cityId;//该区县所属的市级代码
     public int getId() {
         return id;
@@ -27,10 +27,10 @@ public class County extends LitePalSupport {
     public void setCountyCode(int countyCode) {
         this.countyCode = countyCode;
     }
-    public int getWeatherId() {
+    public String getWeatherId() {
         return weatherId;
     }
-    public void setWeatherId(int weatherId) {
+    public void setWeatherId(String weatherId) {
         this.weatherId = weatherId;
     }
     public int getCityId() {
